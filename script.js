511b3486e5e2ec1cd29b0178377d709c94ebb97b
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-24 05:16:26 +0900

    chore: create a function that randomly returns RPS
    *RPS: Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('yo')
+function getComputerChoice() {
+  // initialize an array that contains 3 elements such as "rock", "paper" and "scissors"
+
+  const RockPaperScissors = ['rock', 'paper', 'scissors'];
+
+  // create a variable that randomly access number between 0 and 2
+
+  const randomNum = Math.floor(Math.random() * 3);
+
+  // access the array elements using this variable in square brackets
+
+  console.log(RockPaperScissors[randomNum]);
+}
+
+getComputerChoice()
