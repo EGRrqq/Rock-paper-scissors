d700c3ae890018c0d409aa2aad90c0c90f6f6d53
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-27 12:43:11 +0900

    chore: modify playRound and showScore functions
    chore: add to display results
    chore: change all console.logs to DOM methods

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+const main = document.querySelector("main");
+const p = document.createElement("p");
+const h2 = document.createElement("h2");
+const score = {
+  playerScore: 0,
+  computerScore: 0,
+};
+
 function playRound() {
-  // create variable that stores all btns
   const playButtons = document.querySelectorAll(".btn-choice");
 
-  // loop through these variable
-  // on each iteration add onClick event listener
   playButtons.forEach((btn) =>
     btn.addEventListener("click", (event) => {
-      // pass a function to the event listener that:
-      // - retrieve the innerText of the button
-      // - make innerText case-insensitive
-      // - pass this innerText to 'playerSelection' variable
-
       let playerSelection = btn.innerText.toLowerCase();
       let computerSelection = getComputerChoice();
 
@@ -19,46 +19,44 @@ function playRound() {
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissor" && computerSelection === "paper")
       ) {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        showResult(`You Win! ${playerSelection} beats ${computerSelection}`);
+        showScore();
       } else if (playerSelection === computerSelection) {
-        console.log("Tie! Try your luck in a new round");
+        showResult("Tie! Try your luck in a new round");
+        showScore();
       } else {
-        console.log(`You Lose! ${playerSelection} sucks ${computerSelection}`);
+        showResult(`You Lose! ${playerSelection} sucks ${computerSelection}`);
+        showScore();
       }
     })
   );
 }
 
-console.log(playRound());
+playRound()
+
+function showResult(textContent) {
+  p.textContent = textContent;
+
+  main.appendChild(p);
+}
+
+function showScore() {
+  let results = document.querySelectorAll("p");
+  let lastResults = results[results.length - 1].innerText;
+
+  if (lastResults.includes("Win")) {
+    score.playerScore++;
+  } else if (lastResults.includes("Lose")) {
+    score.computerScore++;
+  }
+
+  h2.textContent = `your score is ${score.playerScore} ${score.computerScore}`;
+  main.appendChild(h2);
+}
 
 function getComputerChoice() {
   const RockPaperScissors = ["rock", "paper", "scissor"];
   const randomNum = Math.floor(Math.random() * 3);
 
   return RockPaperScissors[randomNum];
-}
-
-function getUserChoice() {
-  const usersInput = prompt("rock, paper or scissor?").toLowerCase();
-
-  return usersInput === "rock" ||
-    usersInput === "paper" ||
-    usersInput === "scissor"
-    ? usersInput
-    : getUserChoice();
-}
-
-const score = {
-  playerScore: 0,
-  computerScore: 0,
-};
-
-function scoreCounter(result) {
-  if (result.includes("Win")) {
-    score.playerScore++;
-  } else if (result.includes("Lose")) {
-    score.computerScore++;
-  }
-
-  return `your score is ${score.playerScore} ${score.computerScore}`;
 }
